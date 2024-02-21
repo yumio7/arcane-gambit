7e96192c6e330cb7dd4310cf30c6fc02bee6eccb
Connor Saila <csaila@live.com>
2024-02-21 16:38:06 -0500

    new stuff

--- Arcane Gambit/Assets/Scripts/Opponent.cs
+++ Arcane Gambit/Assets/Scripts/Opponent.cs
@@ -7,9 +7,22 @@ using UnityEngine;
 /// </summary>
 public class Opponent : Player
 {
-    public Opponent(int handSize, int startingChipAmount, int index, string name = "")
-        : base(handSize, startingChipAmount, index, name) { }
+    /// <summary>
+    /// Confidence is the main modifier and is preset for each opponent
+    /// </summary>
+    public int conf { get; }
 
-    
+    /// <summary>
+    /// Optimism is the variable parameter, it changes based on whats going on in the game
+    /// </summary>
+    public int opt { get; private set; }
+
+    public Opponent(int handSize, int startingChipAmount, int index, string name = "", int conf = 5)
+        : base(handSize, startingChipAmount, index, name) 
+    {
+        // TODO: determine what scale this will be on
+        this.conf = conf;
+        this.opt = 50;
+    }
 
 }
