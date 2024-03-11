f07a5ab97c68f454985b542c7532eb1622bf20f4
Connor Saila <csaila@live.com>
2024-03-11 18:23:19 -0400

    pulled numbers out of my ass, probably doesnt work yet

--- Arcane Gambit/Assets/Scripts/Opponent.cs
+++ Arcane Gambit/Assets/Scripts/Opponent.cs
@@ -10,35 +10,67 @@ public class Opponent : Player
     private PokerData _pokerData;
     
     /// <summary>
-    /// Confidence is the main modifier and is preset for each opponent
+    /// Confidence is the main modifier and is preset for each opponent, it's a number between 0 and 10
     /// </summary>
     public int conf { get; }
 
     /// <summary>
-    /// Optimism is the variable parameter, it changes based on whats going on in the game
+    /// Optimism is the variable parameter, its randomly generated before decisions.
     /// </summary>
     public int opt { get; private set; }
 
     public Opponent(int handSize, int startingChipAmount, int index, string name = "", int conf = 5)
         : base(handSize, startingChipAmount, index, PlayerType.AI, name) 
     {
-        // TODO: determine what scale this will be on
         this.conf = conf;
-        opt = 50;
+        opt = 5;
     }
 
     private void calc_bid()
     {
         int bid = 0;
+        // the score from the hand evaluator
         int hand_score = HandEvaluator.EvaluateHand(Hand.Cards);
-        //TODO: turn this number ^ into something between 0 and 1 using confidence and optimism
-        //TODO: calculate optimism
 
+        // normalized hand score between 0 and 1
+        float raw_score = Opponent.normalize_score(hand_score);
+
+        // calculate optimism
+        this.opt = Random.Range(1, 6) + Random.Range(1,6);
+
+        // apply modifiers
+        raw_score *= conf_mod();
+        raw_score *= (this.opt * 0.2);
+
+        if (raw_score > 1.85)
+        {
+            bid = TotalChips;
+        }
+        else if (raw_score >= 1.35)
+        {
+            Raise()
+        }
+        else if (raw_score < 0.25)
+        {
+            Fold();
+        }
+        else 
+        {
+            Match();
+        }
         //TODO: Community card needs to be taken into account
         
-        //FIXME: game info struct could be used to make harder difficulties
-        
-        RespondToBid(bid);
+        // game info struct could be used to make harder difficulties
+    }
+
+    static public float normalize_score(int hand_score)
+    {
+        return hand_score / 8000014;
+    }
+
+    private float conf_mod()
+    {
+        return Math.Log(conf + 5, 10);
     }
 
     public override void BidRequest(int bidToMatch)
