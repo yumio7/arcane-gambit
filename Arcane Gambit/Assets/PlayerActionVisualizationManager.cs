bce6e49d5466f9cc4e5219c4dac9be5638214474
Luca Sandoval <32229145+LucaSandoval@users.noreply.github.com>
2024-03-18 20:51:09 -0400

    basic status message support for debugging

---
+++ Arcane Gambit/Assets/PlayerActionVisualizationManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActionVisualizationManager : MonoBehaviour
+{
+    [Header("Poker Instance")]
+    public Poker poker;
+    [Header("Message Visualization Components")]
+    public WorldMessageVisualizerComponent[] worldMessageVisualizers;
+
+    private List<Player> players_in_game;
+
+    void Start()
+    {
+        players_in_game = new List<Player>();
+        if (poker == null) { Debug.LogWarning("Player UI missing poker reference!"); }
+
+        if (poker != null)
+        {
+            CyclicList<Player> players = poker.Players;
+            for (int i = 0; i < players.Count; i++)
+            {
+                players_in_game.Add(players[i]);
+                players[i].OnPlayerResponse += DisplayPlayerActionResponseMessage;
+                players.Next();
+            }
+        }
+    }
+
+    public void DisplayPlayerActionResponseMessage(Player player, PlayerRequestType requestType, int value)
+    {
+        if (players_in_game.Contains(player) && poker != null)
+        {
+            int index = players_in_game.IndexOf(player);
+            if (index < worldMessageVisualizers.Length)
+            {
+                if (worldMessageVisualizers[index] != null)
+                {
+                    string message = player.Name + " ";
+
+                    switch(requestType)
+                    {
+                        case PlayerRequestType.Bid:
+                            if (value == 0)
+                            {
+                                message += "checked.";
+                            } else if (value == -1)
+                            {
+                                message += "folded.";
+                            } else if (value == poker.CurrentMinBid)
+                            {
+                                message += "matched the bet.";
+                            }
+                            else
+                            {
+                                message += "raised by " + value.ToString() + ".";
+                            }
+                            break;
+                        case PlayerRequestType.Mulligan:
+                            if (value > 0)
+                            {
+                                message += "took " + value + " cards.";
+                            } else
+                            {
+                                message += "didn't take any cards.";
+                            }
+                            break;
+                    }
+
+                    worldMessageVisualizers[index].DisplayMessage(message);
+                }
+            }
+        }
+    }
+}
---
+++ Arcane Gambit/Assets/Scripts/WorldMessageVisualizerComponent.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldMessageVisualizerComponent : MonoBehaviour
+{
+    [Header("UI References")]
+    public GameObject anchor_point;
+    public GameObject messages_parent;
+    public Text message_text;
+    [Header("Visuals")]
+    public float message_display_time = 2f;
+
+    // References
+    private Camera main_camera;
+    private float display_timer;
+
+    void Start()
+    {
+        display_timer = 0;
+        main_camera = Camera.main;
+    }
+
+    // Displays message for a period of time.
+    public void DisplayMessage(string message)
+    {
+        if (message_text != null)
+        {
+            message_text.text = message;
+        }
+        display_timer = message_display_time;
+    }
+
+    void Update()
+    {
+        // Billboarding effect
+        if (main_camera != null && anchor_point != null)
+        {
+            Vector3 cam_pos = main_camera.transform.position;
+            Vector3 look_rot = (anchor_point.transform.position - cam_pos);
+            anchor_point.transform.rotation = Quaternion.LookRotation(look_rot);
+        }
+        // Message display
+        if (message_text != null)
+        {
+            messages_parent.SetActive(display_timer > 0);
+            if (display_timer > 0)
+            {
+                display_timer -= Time.deltaTime;
+            }
+        }
+    }
+}
